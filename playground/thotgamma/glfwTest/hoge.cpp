ea671e59160301f13e22487e92e41e1e249591c7
thotgamma <gamma@e.thotgamma.com>
2017-05-07 20:17:55 +0900

    お片付け

--- playground/thotgamma/glfwTest/hoge.cpp
+++ playground/thotgamma/glfwTest/hoge.cpp
@@ -188,18 +188,6 @@ int main(){
 	// Or, for an ortho camera :
 	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
 	
-	// Camera matrix
-	glm::mat4 View       = glm::lookAt(
-								glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
-								glm::vec3(0,0,0), // and looks at the origin
-								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-						   );
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model      = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
-
-
 
 	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
@@ -207,36 +195,47 @@ int main(){
 		-1.0f,-1.0f,-1.0f,
 		-1.0f,-1.0f, 1.0f,
 		-1.0f, 1.0f, 1.0f,
+
 		 1.0f, 1.0f,-1.0f,
 		-1.0f,-1.0f,-1.0f,
 		-1.0f, 1.0f,-1.0f,
+
 		 1.0f,-1.0f, 1.0f,
 		-1.0f,-1.0f,-1.0f,
 		 1.0f,-1.0f,-1.0f,
+
 		 1.0f, 1.0f,-1.0f,
 		 1.0f,-1.0f,-1.0f,
 		-1.0f,-1.0f,-1.0f,
+
 		-1.0f,-1.0f,-1.0f,
 		-1.0f, 1.0f, 1.0f,
 		-1.0f, 1.0f,-1.0f,
+
 		 1.0f,-1.0f, 1.0f,
 		-1.0f,-1.0f, 1.0f,
 		-1.0f,-1.0f,-1.0f,
+
 		-1.0f, 1.0f, 1.0f,
 		-1.0f,-1.0f, 1.0f,
 		 1.0f,-1.0f, 1.0f,
+
 		 1.0f, 1.0f, 1.0f,
 		 1.0f,-1.0f,-1.0f,
 		 1.0f, 1.0f,-1.0f,
+
 		 1.0f,-1.0f,-1.0f,
 		 1.0f, 1.0f, 1.0f,
 		 1.0f,-1.0f, 1.0f,
+
 		 1.0f, 1.0f, 1.0f,
 		 1.0f, 1.0f,-1.0f,
 		-1.0f, 1.0f,-1.0f,
+
 		 1.0f, 1.0f, 1.0f,
 		-1.0f, 1.0f,-1.0f,
 		-1.0f, 1.0f, 1.0f,
+
 		 1.0f, 1.0f, 1.0f,
 		-1.0f, 1.0f, 1.0f,
 		 1.0f,-1.0f, 1.0f
