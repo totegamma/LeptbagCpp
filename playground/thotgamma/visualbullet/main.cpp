9c177ea53137119ebd71cc960d2f6574dac9323c
thotgamma <gamma@e.thotgamma.com>
2017-05-18 11:15:28 +0900

    makefile書いた

--- playground/thotgamma/visualbullet/main.cpp
+++ playground/thotgamma/visualbullet/main.cpp
@@ -103,7 +103,7 @@ void computeMatricesFromInputs(){
 	float FoV = initialFoV;
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+	ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 300.0f);
 
 	// Camera matrix
 	ViewMatrix = glm::lookAt(
