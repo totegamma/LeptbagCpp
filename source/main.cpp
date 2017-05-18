fdb5c29bedc690fd03d73f76a3dbf95cadce7b32
thotgamma <gamma@e.thotgamma.com>
2017-05-18 15:06:39 +0900

    動的ロードのテストコードを追加

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <random>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
@@ -376,6 +380,31 @@ class dog{
 
 
 int main(){
+
+
+	void *lh = dlopen("plugins/testdll.so", RTLD_LAZY);
+	if (!lh) {
+		fprintf(stderr, "dlopen error: %s\n", dlerror());
+		exit(1);
+	}
+	printf("libdll.so is loaded\n");
+
+	int (*fn)() = (int (*)())dlsym(lh, "dll");
+	char *error = dlerror();
+	if (error) {
+		fprintf(stderr, "dlsym error: %s\n", error);
+		exit(1);
+	}
+	printf("dll() function is found\n");
+
+	(*fn)();
+
+	printf("unloading libdll.so\n");
+	dlclose(lh);
+
+
+
+
 	if (!glfwInit()){
 		std::cout << "glfw init failed...." << std::endl;
 	}
