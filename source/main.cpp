efec839398ccca02db51604da1e923c020027278
thotgamma <gamma@e.thotgamma.com>
2017-05-20 17:52:10 +0900

    発破解体

--- source/main.cpp
+++ source/main.cpp
@@ -224,164 +224,10 @@ btQuaternion btcreateq(double RotationAngle, double RotationAxisX, double Rotati
 }
 
 
-class dog{
-
-	public:
-
-	float dna[20][4] = {};
-	btDiscreteDynamicsWorld* dynamicsWorld;
-
-
-	cubeshapeObject* body;
-	cubeshapeObject* head;
-	cubeshapeObject* muzzle;
-	cubeshapeObject* earLeft;
-	cubeshapeObject* earRight;
-	cubeshapeObject* legFrontLeft;
-	cubeshapeObject* legFrontRight;
-	cubeshapeObject* legBackLeft;
-	cubeshapeObject* legBackRight;
-	cubeshapeObject* tail;
-
-	btHingeConstraint* hinge_body_head;
-	btHingeConstraint* hinge_head_muzzle;
-	btHingeConstraint* hinge_earLeft_head;
-	btHingeConstraint* hinge_earRight_head;
-	btHingeConstraint* hinge_body_legFrontLeft;
-	btHingeConstraint* hinge_body_legFrontRight;
-	btHingeConstraint* hinge_body_legBackLeft;
-	btHingeConstraint* hinge_body_legBackRight;
-	btHingeConstraint* hinge_body_tail;
-
-
-	dog(btDiscreteDynamicsWorld* dynamicsWorld, float x, float y, float z, bool initialDNA){
-
-		this->dynamicsWorld = dynamicsWorld;
-
-
-		//DNAをランダムで初期化する
-		if(initialDNA == true){
-			std::random_device rd;
-			std::mt19937 mt(rd());
-			std::uniform_real_distribution<double> score(-1.57,1.57);
-
-			for(auto elem: dna){
-				elem[0] = score(mt);
-				elem[1] = score(mt);
-				elem[2] = score(mt);
-				elem[3] = score(mt);
-			}
-		}
-
-		spawn(x, y, z);
-
-	}
-
-	void spawn(float x, float y, float z){
-		//犬の体の構造を定義している
-		//キューブで肉体を作る cubeshape::create(位置, 大きさ, 傾き, 重さ, 追加先物理世界);
-		body			= cubeshape::create(glm::vec3(x,     y,     z),		glm::vec3(2, 1, 1),			glm::quat(1, 0, 0, 0), 2,		dynamicsWorld);
-		head			= cubeshape::create(glm::vec3(x+1.4, y,     z),		glm::vec3(0.8, 0.8, 0.8),	glm::quat(1, 0, 0, 0), 0.5,		dynamicsWorld);
-		muzzle			= cubeshape::create(glm::vec3(x+2.1, y-0.2, z),		glm::vec3(0.6, 0.4, 0.4),	glm::quat(1, 0, 0, 0), 0.1,		dynamicsWorld);
-		earLeft			= cubeshape::create(glm::vec3(x+1.4, y+0.5, z-0.2),	glm::vec3(0.2, 0.2, 0.2),	glm::quat(1, 0, 0, 0), 0.05,	dynamicsWorld);
-		earRight		= cubeshape::create(glm::vec3(x+1.4, y+0.5, z+0.2),	glm::vec3(0.2, 0.2, 0.2),	glm::quat(1, 0, 0, 0), 0.05,	dynamicsWorld);
-		legFrontLeft	= cubeshape::create(glm::vec3(x+0.5, y-1,   z-0.4),	glm::vec3(0.2, 1, 0.2),		glm::quat(1, 0, 0, 0), 0.3,		dynamicsWorld);
-		legFrontRight	= cubeshape::create(glm::vec3(x+0.5, y-1,   z+0.4),	glm::vec3(0.2, 1, 0.2),		glm::quat(1, 0, 0, 0), 0.3,		dynamicsWorld);
-		legBackLeft		= cubeshape::create(glm::vec3(x-0.5, y-1,   z-0.4),	glm::vec3(0.2, 1, 0.2),		glm::quat(1, 0, 0, 0), 0.3,		dynamicsWorld);
-		legBackRight	= cubeshape::create(glm::vec3(x-0.5, y-1,   z+0.4),	glm::vec3(0.2, 1, 0.2),		glm::quat(1, 0, 0, 0), 0.3,		dynamicsWorld);
-		tail			= cubeshape::create(glm::vec3(x-1.5, y+0.4, z),		glm::vec3(1, 0.2, 0.2),		glm::quat(1, 0, 0, 0), 0.2,		dynamicsWorld);
-
-		//肉体同士を関節で接続する	btHingeConstraint(物体A, 物体B, 物体A上の位置, 物体B上の位置, ヒンジの軸の方向);
-		hinge_body_head = new btHingeConstraint(*(body->body), *(head->body), btVector3(1, 0, 0), btVector3(-0.4, 0, 0), btVector3(0, 0, 1), btVector3(0, 0, 1));
-		hinge_body_head->setLimit(-3.14/6, 3.14/6);
-		dynamicsWorld->addConstraint(hinge_body_head, true);
-
-		hinge_head_muzzle = new btHingeConstraint(*(head->body), *(muzzle->body), btVector3(0.4, -0.2, 0), btVector3(-0.3, 0, 0), btVector3(1, 0, 0), btVector3(1, 0, 0));
-		hinge_head_muzzle->setLimit(0, 0);
-		dynamicsWorld->addConstraint(hinge_head_muzzle, true);
-
-		hinge_earLeft_head = new btHingeConstraint(*(earLeft->body), *(head->body), btVector3(0, -0.1, 0), btVector3(0, 0.4, -0.2), btVector3(1, 0, 0), btVector3(1, 0, 0));
-		hinge_earLeft_head->setLimit(0, 0);
-		dynamicsWorld->addConstraint(hinge_earLeft_head, true);
-
-		hinge_earRight_head = new btHingeConstraint(*(earRight->body), *(head->body), btVector3(0, -0.1, 0), btVector3(0, 0.4, 0.2), btVector3(1, 0, 0), btVector3(1, 0, 0));
-		hinge_earRight_head->setLimit(0, 0);
-		dynamicsWorld->addConstraint(hinge_earRight_head, true);
-
-
-		hinge_body_legFrontLeft = new btHingeConstraint(*(body->body), *(legFrontLeft->body), btVector3(0.5, -0.5, -0.4), btVector3(0, 0.5, 0.0), btVector3(0, 0, 1), btVector3(0, 0, 1));
-		hinge_body_legFrontLeft->setLimit(-3.14/2, 3.14/2);
-		dynamicsWorld->addConstraint(hinge_body_legFrontLeft, true);
-
-		hinge_body_legFrontRight = new btHingeConstraint(*(body->body), *(legFrontRight->body), btVector3(0.5, -0.5, 0.4), btVector3(0, 0.5, 0.0), btVector3(0, 0, 1), btVector3(0, 0, 1));
-		hinge_body_legFrontRight->setLimit(-3.14/2, 3.14/2);
-		dynamicsWorld->addConstraint(hinge_body_legFrontRight, true);
-
-		hinge_body_legBackLeft = new btHingeConstraint(*(body->body), *(legBackLeft->body), btVector3(-0.5, -0.5, -0.4), btVector3(0, 0.5, 0.0), btVector3(0, 0, 1), btVector3(0, 0, 1));
-		hinge_body_legBackLeft->setLimit(-3.14/2, 3.14/2);
-		dynamicsWorld->addConstraint(hinge_body_legBackLeft, true);
-
-		hinge_body_legBackRight = new btHingeConstraint(*(body->body), *(legBackRight->body), btVector3(-0.5, -0.5, 0.4), btVector3(0, 0.5, 0.0), btVector3(0, 0, 1), btVector3(0, 0, 1));
-		hinge_body_legBackRight->setLimit(-3.14/2, 3.14/2);
-		dynamicsWorld->addConstraint(hinge_body_legBackRight, true);
-
-		hinge_body_tail = new btHingeConstraint(*(body->body), *(tail->body), btVector3(-1, 0.4, 0), btVector3(0.5, 0, 0.0), btVector3(0, 0, 1), btVector3(0, 0, 1));
-		hinge_body_tail->setLimit(-3.14/3, 3.14/3);
-		dynamicsWorld->addConstraint(hinge_body_tail, true);
-
-		//足の関節にモーターをつけている
-		hinge_body_legFrontLeft->enableMotor(true);
-		hinge_body_legFrontLeft->setMaxMotorImpulse(2);
-		hinge_body_legFrontRight->enableMotor(true);
-		hinge_body_legFrontRight->setMaxMotorImpulse(2);
-		hinge_body_legBackLeft->enableMotor(true);
-		hinge_body_legBackLeft->setMaxMotorImpulse(2);
-		hinge_body_legBackRight->enableMotor(true);
-		hinge_body_legBackRight->setMaxMotorImpulse(2);
-	}
-
-
-	//シーケンス番号に対応するDNAに記録されている角度まで足を動かす
-	void move(int sequence){
-		hinge_body_legFrontLeft->setMotorTarget(dna[sequence][0], 0.3);
-		hinge_body_legFrontRight->setMotorTarget(dna[sequence][1], 0.3);
-		hinge_body_legBackLeft->setMotorTarget(dna[sequence][2], 0.3);
-		hinge_body_legBackRight->setMotorTarget(dna[sequence][3], 0.3);
-	}
-
-	void destroy(){
-		dynamicsWorld->removeConstraint(hinge_body_head);
-		dynamicsWorld->removeConstraint(hinge_body_head);
-		dynamicsWorld->removeConstraint(hinge_head_muzzle);
-		dynamicsWorld->removeConstraint(hinge_earLeft_head);
-		dynamicsWorld->removeConstraint(hinge_earRight_head);
-		dynamicsWorld->removeConstraint(hinge_body_legFrontLeft);
-		dynamicsWorld->removeConstraint(hinge_body_legFrontRight);
-		dynamicsWorld->removeConstraint(hinge_body_legBackLeft);
-		dynamicsWorld->removeConstraint(hinge_body_legBackRight);
-		dynamicsWorld->removeConstraint(hinge_body_tail);
-
-		body->destroy();
-		head->destroy();
-		muzzle->destroy();
-		earLeft->destroy();
-		earRight->destroy();
-		legFrontLeft->destroy();
-		legFrontRight->destroy();
-		legBackLeft->destroy();
-		legBackRight->destroy();
-		tail->destroy();
-	}
-
-
-};
-
-
-
-
 int main(){
 
 
+	/*
 	void *lh = dlopen("plugins/testdll.so", RTLD_LAZY);
 	if (!lh) {
 		fprintf(stderr, "dlopen error: %s\n", dlerror());
@@ -401,8 +247,7 @@ int main(){
 
 	printf("unloading libdll.so\n");
 	dlclose(lh);
-
-
+	*/
 
 
 	if (!glfwInit()){
@@ -485,32 +330,6 @@ int main(){
 
 
 
-	float topRecord = 0;
-
-	int timerDivisor = 0;
-	int time = 0; //0.1秒ごとに増えるんじゃない？知らんけど。
-	int generation = 0;
-	int sequence = 0;
-
-	std::vector<dog*> doglist;
-
-	//0世代目の犬。全部ランダム。
-	for(int i = 0; i < 100; i++){
-		doglist.push_back(new dog(dynamicsWorld, 0, 1.5, -5*i, true));
-	}
-
-
-	//犬の交配計算に使う乱数生成器
-	std::random_device rd;
-	std::mt19937 mt(rd());
-	std::uniform_int_distribution<int> coin(0,1);
-	std::uniform_int_distribution<int> RNDnumOfAttack(0,10);
-	std::uniform_int_distribution<int> RNDnumOfRow(0,3);
-	std::uniform_int_distribution<int> RNDnumOfColumn(0,19);
-	std::uniform_real_distribution<double> score(-1.57,1.57);
-
-
-
 	//毎フレームごとにこの中が実装される。
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == GL_FALSE){
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -520,116 +339,6 @@ int main(){
 
 		//物理演算1ステップ進める
 		dynamicsWorld->stepSimulation(1 / 60.f, 10);
-
-		//時間
-		if(timerDivisor++ == 6){
-			sequence = (sequence+1)%20; //犬のポーズを変更する
-			timerDivisor = 0;
-
-			//犬を動かす
-			for(auto elem: doglist){
-				elem->move(sequence);
-			}
-
-			time ++;
-		}
-
-		//世代終わり
-		if(time == 30 + generation*2){
-
-			//まずは優良個体を調べる
-			float firstdna[20][4];
-			float seconddna[20][4];
-
-			float current1stMax = -128; //まぁ-128も後退することはないだろうなという気持ち(これが最低値だろう)
-			float current2ndMax = -128; //まぁ-128も後退することはないだろうなという気持ち(これが最低値だろう)
-
-			for(auto elem: doglist){
-				btTransform transform;
-				elem->muzzle->body->getMotionState()->getWorldTransform(transform);
-				btVector3 pos = transform.getOrigin();
-
-				//std::cout << pos.getX() << std::endl;
-
-				//1番と2番を求める
-				if(pos.getX() > current1stMax){
-					current2ndMax = current1stMax;
-					current1stMax = pos.getX();
-					memcpy(seconddna, firstdna, sizeof(float)*20*4);
-					memcpy(firstdna, elem->dna, sizeof(float)*20*4);
-				}else if(pos.getX() > current2ndMax){
-					current2ndMax = pos.getX();
-					memcpy(seconddna, elem->dna, sizeof(float)*20*4);
-				}
-			}
-
-			if(current1stMax > topRecord){
-				topRecord = current1stMax;
-				std::cout << "New Record! " << topRecord << "m" << std::endl;
-				std::cout << std::endl;
-				std::cout << "---DNA DATA--------------------" << std::endl;
-				for(int a = 0; a < 20; a++){
-					for(int b = 0; b < 4; b++){
-						std::cout << firstdna[a][b] << ", ";
-					}
-					std::cout << std::endl;
-				}
-				std::cout << "-------------------------------" << std::endl;
-				std::cout << std::endl;
-			}
-
-
-			//今の犬を削除
-			while(doglist.size() > 0){
-				doglist.back()->destroy();
-				doglist.pop_back();
-			}
-
-
-			//新しく犬をつくる
-
-			dog* newdog;
-
-			//1番の犬
-			newdog = new dog(dynamicsWorld, 0, 1.5, -5*0, false);
-			memcpy(newdog->dna, firstdna, sizeof(float)*20*4);
-
-			doglist.push_back(newdog);
-
-			//2番の犬
-			newdog = new dog(dynamicsWorld, 0, 1.5, -5*1, false);
-			memcpy(newdog->dna, seconddna, sizeof(float)*20*4);
-			doglist.push_back(newdog);
-
-			//残りの犬
-			for(int i = 2; i < 100; i++){
-				newdog = new dog(dynamicsWorld, 0, 1.5, -5*i, false);
-
-				//交叉
-				for(int dnaIndex = 0; dnaIndex < 20; dnaIndex++){
-					if(coin(mt) == 0){
-						memcpy(newdog->dna[dnaIndex], firstdna[dnaIndex], sizeof(float)*4);
-					}else{
-						memcpy(newdog->dna[dnaIndex], seconddna[dnaIndex], sizeof(float)*4);
-					}
-				}
-
-				//突然変異の回数
-				int numOfAttack = RNDnumOfAttack(mt);
-				
-				for(int j = 0; j < numOfAttack; j++){
-					newdog->dna[RNDnumOfColumn(mt)][RNDnumOfRow(mt)] = score(mt);
-				}
-
-				doglist.push_back(newdog);
-			}
-
-			time = 0;
-			generation++;
-			std::cout << "generation" << generation << " begin" << std::endl;
-		}
-
-
 
 
 		//OpenGL描画
