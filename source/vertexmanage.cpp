293c0504d27986e6d0f94b09ffd1f4686620097e
thotgamma <gamma@e.thotgamma.com>
2017-06-01 10:16:16 +0900

    Vertexの入出力をすべてvectorで行うようにした

--- source/vertexmanage.cpp
+++ source/vertexmanage.cpp
@@ -10,17 +10,19 @@ void initVBO(){
 	glGenBuffers(1, &vertexBufferObject);
 }
 
-void registervertex(vertex input[], GLuint* arrayaddr, int length){
+void registervertex(std::vector<vertex>* input, std::vector<GLuint>* arrayaddr){
 
-	for(int i = 0; i < length; i++){
-		auto itr = std::find(vertexBufferArray.begin(), vertexBufferArray.end(), input[i]);
-		if( itr != vertexBufferArray.end() ) {
-			arrayaddr[i] = std::distance(vertexBufferArray.begin(), itr);
+	for(auto elem: *input){
+		auto itr = std::find(vertexBufferArray.begin(), vertexBufferArray.end(), elem);
+		if( itr != vertexBufferArray.end() ){
+			arrayaddr->push_back(std::distance(vertexBufferArray.begin(), itr));
 		}else{
-			vertexBufferArray.push_back(input[i]);
-			arrayaddr[i] = vertexBufferArray.size() -1;
+			vertexBufferArray.push_back(elem);
+			arrayaddr->push_back(vertexBufferArray.size() -1);
 		}
 	}
+
+
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
 	glBufferData(GL_ARRAY_BUFFER, vertexBufferArray.size() * sizeof(vertex), &vertexBufferArray[0], GL_STATIC_DRAW);
 }
