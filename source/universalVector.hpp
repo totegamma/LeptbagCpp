4d4ed446ce5d5037b1ff4d60afa2bea0d142d1fe
thotgamma <gamma@e.thotgamma.com>
2017-06-02 19:48:33 +0900

    簡単にvec3とquatだけ書いた

---
+++ source/universalVector.hpp
@@ -0,0 +1,66 @@
+#ifndef UNIVERSAL_VECTOR_HPP 
+#define UNIVERSAL_VECTOR_HPP
+
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <btBulletDynamicsCommon.h>
+
+
+class vec3{
+
+	public:
+
+	float x;
+	float y;
+	float z;
+
+	vec3();
+	vec3(float x, float y, float z);
+	btVector3 toBullet();
+	glm::vec3 toGlm();
+};
+
+extern "C" createVec3(float x, float y, float z);
+
+class quarternion{
+
+	public:
+
+	float w;
+	float x;
+	float y;
+	float z;
+
+	quarternion(){
+		w = x = y = z = 0;
+	}
+
+	quarternion(float w, float x, float y, float z){
+		this->w = w;
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+
+	btQuaternion toBullet(){
+		return btQuaternion(w, x, y, z);
+	}
+
+	glm::quat toGlm(){
+		return glm::quat(x, y, z, w);
+	}
+}
+
+
+extern "C" createQuat(float w, float x, float y, float z);
+
+
+#endif
