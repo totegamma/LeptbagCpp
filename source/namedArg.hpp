5d3f9926a9e7257c48efd5e39a2aca98be36fa76
thotgamma <gamma@e.thotgamma.com>
2017-07-13 00:20:44 +0900

    名前付き引数を使うためのツールを追加
    まだテストしてない

---
+++ source/namedArg.hpp
@@ -0,0 +1,36 @@
+#include <tuple>
+
+template <typename Name, typename T>
+struct argumentContainer{
+	T value;
+};
+
+template <typename Char, Char... CS>
+struct argumentWrapper{
+	template<typename T>
+	auto operator =(T t){
+		return argumentContainer<argumentWrapper, T>{t};
+	}
+};
+
+template<typename Char, Char... CS>
+auto operator "" _arg(){
+	return argumentWrapper<Char, CS...>{};
+}
+
+
+template <typename Char, Char... CS>
+struct getArgumentType{
+	using type = argumentWrapper<Char, CS...>;
+};
+
+template <typename Char, Char... CS>
+auto operator"" _argunwrap(){
+	return getArgumentType<Char, CS...>();
+}
+
+template<typename T, typename Name, typename... Args, typename result = typename Name::type>
+auto getArg(Name, Args... args){
+	return std::get<argumentContainer<result, T>>(std::make_tuple(args...)).value;
+}
+
