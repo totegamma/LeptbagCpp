981397bf36f16cf0b4059e6a47a290e645c9df5f
sanamilon <s1611408@u.tsukuba.ac.jp>
2017-08-05 20:52:46 +0900

    add "genericConstraint" in fpmExporter.py & chorodog.d
    それに応じてヒンジの”ObjectType”を”hingeConstraint”に変更．

--- blenderPlugin/fpmExporter.py
+++ blenderPlugin/fpmExporter.py
@@ -54,87 +54,140 @@ class fpmExporter(bpy.types.Operator, ExportHelper):
             obj = bpy.data.objects[keyname]
             if obj.type == "EMPTY":
 
-                hingeLocation = np.array([obj.location[0], obj.location[1], obj.location[2]], dtype=float)
-                hingeQuat = np.array([obj.rotation_quaternion[0], obj.rotation_quaternion[1], obj.rotation_quaternion[2], obj.rotation_quaternion[3]], dtype=float)
-                hingeQuatInv = getQuatInv(hingeQuat)
 
-                #ヒンジの相対角度は回転前のobj1, obj2に対するものなので，それを補正
-                obj1Location = np.array([obj.rigid_body_constraint.object1.location[0],
-                                         obj.rigid_body_constraint.object1.location[1],
-                                         obj.rigid_body_constraint.object1.location[2]],
-                                        dtype=float)
-                #クォータニオン
-                obj1Quat = np.array([obj.rigid_body_constraint.object1.rotation_quaternion[0],
-                                     obj.rigid_body_constraint.object1.rotation_quaternion[1],
-                                     obj.rigid_body_constraint.object1.rotation_quaternion[2],
-                                     obj.rigid_body_constraint.object1.rotation_quaternion[3]],
-                                    dtype=float)
+                if obj.rigid_body_constraint.type == "HINGE":
 
-                #ヒンジの相対角度は回転前のobj2, obj2に対するものなので，それを補正
-                obj2Location = np.array([obj.rigid_body_constraint.object2.location[0],
-                                         obj.rigid_body_constraint.object2.location[1],
-                                         obj.rigid_body_constraint.object2.location[2]],
-                                        dtype=float)
-                #クォータニオン
-                obj2Quat = np.array([obj.rigid_body_constraint.object2.rotation_quaternion[0],
-                                     obj.rigid_body_constraint.object2.rotation_quaternion[1],
-                                     obj.rigid_body_constraint.object2.rotation_quaternion[2],
-                                     obj.rigid_body_constraint.object2.rotation_quaternion[3]],
-                                    dtype=float)
+                        hingeLocation = np.array([obj.location[0], obj.location[1], obj.location[2]], dtype=float)
+                        hingeQuat = np.array([obj.rotation_quaternion[0], obj.rotation_quaternion[1], obj.rotation_quaternion[2], obj.rotation_quaternion[3]], dtype=float)
+                        hingeQuatInv = getQuatInv(hingeQuat)
 
-                obj1Location = hingeLocation - obj1Location
-                obj1Location = quaternion(obj1Location, obj1Quat)
+                        #ヒンジの相対角度は回転前のobj1, obj2に対するものなので，それを補正
+                        obj1Location = np.array([obj.rigid_body_constraint.object1.location[0],
+                            obj.rigid_body_constraint.object1.location[1],
+                            obj.rigid_body_constraint.object1.location[2]],
+                            dtype=float)
+                        #クォータニオン
+                        obj1Quat = np.array([obj.rigid_body_constraint.object1.rotation_quaternion[0],
+                            obj.rigid_body_constraint.object1.rotation_quaternion[1],
+                            obj.rigid_body_constraint.object1.rotation_quaternion[2],
+                            obj.rigid_body_constraint.object1.rotation_quaternion[3]],
+                            dtype=float)
 
-                obj2Location = hingeLocation - obj2Location
-                obj2Location = quaternion(obj2Location, obj2Quat)
+                        #ヒンジの相対角度は回転前のobj2, obj2に対するものなので，それを補正
+                        obj2Location = np.array([obj.rigid_body_constraint.object2.location[0],
+                            obj.rigid_body_constraint.object2.location[1],
+                            obj.rigid_body_constraint.object2.location[2]],
+                            dtype=float)
+                        #クォータニオン
+                        obj2Quat = np.array([obj.rigid_body_constraint.object2.rotation_quaternion[0],
+                            obj.rigid_body_constraint.object2.rotation_quaternion[1],
+                            obj.rigid_body_constraint.object2.rotation_quaternion[2],
+                            obj.rigid_body_constraint.object2.rotation_quaternion[3]],
+                            dtype=float)
 
-                hingeObj1Axis = np.array([ 0.0, 0.0, 1.0 ], dtype=float)
-                hingeObj2Axis = np.array([ 0.0, 0.0, 1.0 ], dtype=float)
+                        obj1Location = hingeLocation - obj1Location
+                        obj1Location = quaternion(obj1Location, obj1Quat)
 
-                hingeObj1Axis = quaternion(hingeObj1Axis, hingeQuatInv)
-                hingeObj1Axis = quaternion(hingeObj1Axis, obj1Quat)
+                        obj2Location = hingeLocation - obj2Location
+                        obj2Location = quaternion(obj2Location, obj2Quat)
 
-                hingeObj2Axis = quaternion(hingeObj2Axis, hingeQuatInv)
-                hingeObj2Axis = quaternion(hingeObj2Axis, obj2Quat)
+                        hingeObj1Axis = np.array([ 0.0, 0.0, 1.0 ], dtype=float)
+                        hingeObj2Axis = np.array([ 0.0, 0.0, 1.0 ], dtype=float)
 
+                        hingeObj1Axis = quaternion(hingeObj1Axis, hingeQuatInv)
+                        hingeObj1Axis = quaternion(hingeObj1Axis, obj1Quat)
 
-                fo.write("\t{\n")
-                fo.write("\t\t\"objectType\":\"constraint\",\n")
-                fo.write("\t\t\"name\":\"%s\",\n" % keyname)
-                fo.write("\t\t\"constraintType\":\"%s\",\n" % obj.rigid_body_constraint.type)
-                fo.write("\t\t\"enabled\":\"%s\",\n" % obj.rigid_body_constraint.enabled)
-
-                fo.write("\t\t\"object1\":\"%s\",\n" % obj.rigid_body_constraint.object1.name)
-                fo.write("\t\t\"object1xpos\":%f,\n" % obj1Location[1])
-                fo.write("\t\t\"object1ypos\":%f,\n" % obj1Location[2])
-                fo.write("\t\t\"object1zpos\":%f,\n" % obj1Location[0])
-
-                fo.write("\t\t\"object2\":\"%s\",\n" % obj.rigid_body_constraint.object2.name)
-                fo.write("\t\t\"object2xpos\":%f,\n" % obj2Location[1])
-                fo.write("\t\t\"object2ypos\":%f,\n" % obj2Location[2])
-                fo.write("\t\t\"object2zpos\":%f,\n" % obj2Location[0])
-
-                fo.write("\t\t\"xaxs1\":%f,\n" % hingeObj1Axis[1])
-                fo.write("\t\t\"yaxs1\":%f,\n" % hingeObj1Axis[2])
-                fo.write("\t\t\"zaxs1\":%f,\n" % hingeObj1Axis[0])
-
-                fo.write("\t\t\"xaxs2\":%f,\n" % hingeObj2Axis[1])
-                fo.write("\t\t\"yaxs2\":%f,\n" % hingeObj2Axis[2])
-                fo.write("\t\t\"zaxs2\":%f,\n" % hingeObj2Axis[0])
-
-                fo.write("\t\t\"wqat\":%f,\n" % hingeQuat[0])
-                fo.write("\t\t\"xqat\":%f,\n" % hingeQuat[1])
-                fo.write("\t\t\"yqat\":%f,\n" % hingeQuat[3])
-                fo.write("\t\t\"zqat\":%f,\n" % (-1.0*hingeQuat[2]))
-
-                fo.write("\t\t\"useLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_z)
-                fo.write("\t\t\"limitLower\":%f,\n" % obj.rigid_body_constraint.limit_ang_z_lower)
-                fo.write("\t\t\"limitUpper\":%f\n" % obj.rigid_body_constraint.limit_ang_z_upper)
-                fo.write("\t},\n")
+                        hingeObj2Axis = quaternion(hingeObj2Axis, hingeQuatInv)
+                        hingeObj2Axis = quaternion(hingeObj2Axis, obj2Quat)
 
 
+                        fo.write("\t{\n")
+                        fo.write("\t\t\"objectType\":\"hingeConstraint\",\n")
+                        fo.write("\t\t\"name\":\"%s\",\n" % keyname)
+                        fo.write("\t\t\"constraintType\":\"%s\",\n" % obj.rigid_body_constraint.type)
+                        fo.write("\t\t\"enabled\":\"%s\",\n" % obj.rigid_body_constraint.enabled)
+
+                        fo.write("\t\t\"object1\":\"%s\",\n" % obj.rigid_body_constraint.object1.name)
+                        fo.write("\t\t\"object1xpos\":%f,\n" % obj1Location[1])
+                        fo.write("\t\t\"object1ypos\":%f,\n" % obj1Location[2])
+                        fo.write("\t\t\"object1zpos\":%f,\n" % obj1Location[0])
+
+                        fo.write("\t\t\"object2\":\"%s\",\n" % obj.rigid_body_constraint.object2.name)
+                        fo.write("\t\t\"object2xpos\":%f,\n" % obj2Location[1])
+                        fo.write("\t\t\"object2ypos\":%f,\n" % obj2Location[2])
+                        fo.write("\t\t\"object2zpos\":%f,\n" % obj2Location[0])
+
+                        fo.write("\t\t\"xaxs1\":%f,\n" % hingeObj1Axis[1])
+                        fo.write("\t\t\"yaxs1\":%f,\n" % hingeObj1Axis[2])
+                        fo.write("\t\t\"zaxs1\":%f,\n" % hingeObj1Axis[0])
+
+                        fo.write("\t\t\"xaxs2\":%f,\n" % hingeObj2Axis[1])
+                        fo.write("\t\t\"yaxs2\":%f,\n" % hingeObj2Axis[2])
+                        fo.write("\t\t\"zaxs2\":%f,\n" % hingeObj2Axis[0])
+
+                        fo.write("\t\t\"wqat\":%f,\n" % hingeQuat[0])
+                        fo.write("\t\t\"xqat\":%f,\n" % hingeQuat[1])
+                        fo.write("\t\t\"yqat\":%f,\n" % hingeQuat[3])
+                        fo.write("\t\t\"zqat\":%f,\n" % (-1.0*hingeQuat[2]))
+
+                        fo.write("\t\t\"useLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_z)
+                        fo.write("\t\t\"limitLower\":%f,\n" % obj.rigid_body_constraint.limit_ang_z_lower)
+                        fo.write("\t\t\"limitUpper\":%f\n" % obj.rigid_body_constraint.limit_ang_z_upper)
+                        fo.write("\t},\n")
 
 
+
+
+                elif obj.rigid_body_constraint.type == "GENERIC":
+
+
+
+                        fo.write("\t{\n")
+                        fo.write("\t\t\"objectType\":\"genericConstraint\",\n")
+                        fo.write("\t\t\"name\":\"%s\",\n" % keyname)
+                        fo.write("\t\t\"constraintType\":\"%s\",\n" % obj.rigid_body_constraint.type)
+                        fo.write("\t\t\"enabled\":\"%s\",\n" % obj.rigid_body_constraint.enabled)
+
+                        fo.write("\t\t\"xpos\":\"%s\",\n" % obj.location[1])
+                        fo.write("\t\t\"ypos\":\"%s\",\n" % obj.location[2])
+                        fo.write("\t\t\"zpos\":\"%s\",\n" % obj.location[0])
+
+                        fo.write("\t\t\"wqat\":%f,\n" % obj.rotation_quaternion[0])
+                        fo.write("\t\t\"xqat\":%f,\n" % obj.rotation_quaternion[2])
+                        fo.write("\t\t\"yqat\":%f,\n" % obj.rotation_quaternion[3])
+                        fo.write("\t\t\"zqat\":%f,\n" % obj.rotation_quaternion[1])
+
+                        fo.write("\t\t\"object1\":\"%s\",\n" % obj.rigid_body_constraint.object1.name)
+                        fo.write("\t\t\"object1xpos\":%f,\n" % obj.rigid_body_constraint.object1.location[1])
+                        fo.write("\t\t\"object1ypos\":%f,\n" % obj.rigid_body_constraint.object1.location[2])
+                        fo.write("\t\t\"object1zpos\":%f,\n" % obj.rigid_body_constraint.object1.location[0])
+
+                        fo.write("\t\t\"object2\":\"%s\",\n" % obj.rigid_body_constraint.object2.name)
+                        fo.write("\t\t\"object2xpos\":%f,\n" % obj.rigid_body_constraint.object2.location[1])
+                        fo.write("\t\t\"object2ypos\":%f,\n" % obj.rigid_body_constraint.object2.location[2])
+                        fo.write("\t\t\"object2zpos\":%f,\n" % obj.rigid_body_constraint.object2.location[0])
+
+
+                        fo.write("\t\t\"useXAngLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_y)
+                        fo.write("\t\t\"useYAngLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_z)
+                        fo.write("\t\t\"useZAngLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_x)
+                        fo.write("\t\t\"xAngLimitLower\":%f,\n" % obj.rigid_body_constraint.limit_ang_y_lower)
+                        fo.write("\t\t\"yAngLimitLower\":%f,\n" % obj.rigid_body_constraint.limit_ang_z_lower)
+                        fo.write("\t\t\"zAngLimitLower\":%f,\n" % obj.rigid_body_constraint.limit_ang_x_lower)
+                        fo.write("\t\t\"xAngLimitUpper\":%f,\n" % obj.rigid_body_constraint.limit_ang_y_upper)
+                        fo.write("\t\t\"yAngLimitUpper\":%f,\n" % obj.rigid_body_constraint.limit_ang_z_upper)
+                        fo.write("\t\t\"zAngLimitUpper\":%f,\n" % obj.rigid_body_constraint.limit_ang_x_upper)
+
+                        fo.write("\t\t\"useXLinLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_y)
+                        fo.write("\t\t\"useYLinLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_z)
+                        fo.write("\t\t\"useZLinLimit\":\"%s\",\n" % obj.rigid_body_constraint.use_limit_ang_x)
+                        fo.write("\t\t\"xLinLimitLower\":%f,\n" % obj.rigid_body_constraint.limit_lin_y_lower)
+                        fo.write("\t\t\"yLinLimitLower\":%f,\n" % obj.rigid_body_constraint.limit_lin_z_lower)
+                        fo.write("\t\t\"zLinLimitLower\":%f,\n" % obj.rigid_body_constraint.limit_lin_x_lower)
+                        fo.write("\t\t\"xLinLimitUpper\":%f,\n" % obj.rigid_body_constraint.limit_lin_y_upper)
+                        fo.write("\t\t\"yLinLimitUpper\":%f,\n" % obj.rigid_body_constraint.limit_lin_z_upper)
+                        fo.write("\t\t\"zLinLimitUpper\":%f,\n" % obj.rigid_body_constraint.limit_lin_x_upper)
+                        fo.write("\t},\n")
 
 
 
