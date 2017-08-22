7a2cd24c3ceaca5c997aa76f6a4750d81fc64e31
thotgamma <gamma@e.thotgamma.com>
2017-08-22 12:26:04 +0900

    作業ファイルの削除

--- friends/learningsimpledog_legacy/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TARGET="learningsimpledog"
-
-cp $TARGET".friends" "../../source/friends/"
-cd ../../source
-./japari_park
-cd "../friends/"$TARGET
-rm "../../source/friends/"$TARGET".friends"
