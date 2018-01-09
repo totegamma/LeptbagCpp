d22ef7ae67fce017ac013091b8afc586ba010c6f
thotgamma <gamma@e.thotgamma.com>
2018-01-09 20:16:26 +0900

    init minecraftCreativeCamera.friends

---
+++ friends/minecraftCreativeCamera/source/main.d
@@ -0,0 +1,35 @@
+import core.stdc.stdio;
+import std.stdio;
+import core.runtime;
+import std.random;
+import std.math;
+import std.algorithm;
+
+
+import japariSDK.japarilib;
+import dlib.math.vector;
+import dlib.math.quaternion;
+
+
+
+//ApplicationInterface----------------------
+
+extern (C) void init(){
+	rt_init();
+
+	getPlaneShape().generate(parameterPack(
+									param("position", Vector3f(0, 0, 0)),
+									param("scale",    Vector3f(1, 1, 1)),
+									param("face",     Vector3f(0, 1, 0)),
+									param("rotation", Quaternionf(0, 0, 0, 1)),
+									param("mass",     0.0f)));
+
+
+}
+
+
+extern (C) void tick(){
+
+}
+
+//------------------------------------------
