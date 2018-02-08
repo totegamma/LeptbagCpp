47f41a4fbe7f7de53598010c578950b9ac7cfe4d
thotgamma <gamma@e.thotgamma.com>
2018-02-09 00:09:56 +0900

    cleanup

--- friends/simpleWolf(deprecated)/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TARGET="simplewolf"
-
-cp $TARGET".friends" "../../source/friends/"
-cd ../../source
-./japari_park
-cd "../friends/"$TARGET
-rm "../../source/friends/"$TARGET".friends"
