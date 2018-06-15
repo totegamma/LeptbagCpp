6e68df91ab4eea533b46e37399ddbbf017758a4c
thotgamma <github@gammalab.net>
2018-06-15 15:19:03 +0900

    planktowerを追加

--- friends(C++)/simpleFloor/simpleFloor.cpp
+++ friends(C++)/simpleFloor/simpleFloor.cpp
@@ -11,11 +11,11 @@ void init() {
 	std::cout << "plugin loaded!!!" << std::endl;
 
 	getPlaneshape()->generate(paramPack(
-								param("position", Eigen::Vector3f(0, -3, 0)),
+								param("position", Eigen::Vector3f(0, 0, 0)),
 								param("scale",    Eigen::Vector3f(1, 1, 1)),
 								param("face",     Eigen::Vector3f(0, 1, 0)),
 								param("rotation", Eigen::Quaternionf(1, 0, 0, 0)),
-								param("mass",     1.0f)));
+								param("mass",     0.0f)));
 
 }
 
